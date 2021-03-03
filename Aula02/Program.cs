eb7d188cd23e5462c2c5886fd12e2546eae27e87
pleiterson <pleiterson@gmail.com>
2021-03-03 08:40:34 -0300

    concluido a aula02

---
+++ Aula02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CFB Cursos");
+            if (args.GetLength(0) > 0) {
+                Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
